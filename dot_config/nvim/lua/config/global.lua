a557ea69ccd0914fe6c88413661d848c10d364d8
Xiaotian Guo <guoxiaotian@gmail.com>
2025-10-25 13:46:35 -0700

    change leader key to space

--- dot_config/nvim/lua/config/global.lua
+++ dot_config/nvim/lua/config/global.lua
@@ -4,7 +4,7 @@
 -- NOTE: Must happen before loading plugins.
 --
 
-vim.g.mapleader = ","  -- Sets leader to Space
-vim.g.maplocalleader = ","
+vim.g.mapleader = " "  -- Sets leader to Space
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true

63db6c270f78cb46737b2256e33f7580b904e6bb
Xiaotian Guo <guoxiaotian@gmail.com>
2025-10-24 21:58:25 -0700

    removed nvim-settings.lua, as we split it into separated files

--- dot_config/nvim/lua/nvim-settings.lua
+++
@@ -1,9 +0,0 @@
-
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2") -- controls auto-identation
-
-vim.g.mapleader = ","  -- Sets leader to Space
-
-vim.opt.number = true          -- Show absolute line numbers
--- vim.opt.relativenumber = true

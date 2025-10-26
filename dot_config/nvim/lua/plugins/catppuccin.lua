6f817e008748058a2231be0b81b639310d3cf57e
Xiaotian Guo <guoxiaotian@gmail.com>
2025-10-25 19:35:46 -0700

    tweak the background color a bit

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -7,7 +7,7 @@ return {
 			auto_integrations = true,
 			color_overrides = {
 			mocha = {
-					base = "#111111",
+					base = "#141414",
 					mantle = "#0d0d0d",
 					crust = "#000000",
 				},

3ff8e77edf005935ee56439b44bfc39a06ad989d
Xiaotian Guo <guoxiaotian@gmail.com>
2025-10-26 19:57:17 -0700

    tweak lualine color

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,47 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-		require('lualine').setup({
-			options = { theme = 'codedark' }
-		})
-	end
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local colors = {
+      gray       = '#3C3C3C',
+      teak       = '#D75B7A',
+      orange     = '#63B4D6',
+      yellow     = '#D79920',
+      black      = '#262626',
+      white      = '#D4D4D4',
+      green      = '#A1D6B2',
+      text       = "#E4E4DD",
+      text_dark  = "#54622D",
+    }
+
+    theme = {
+      normal = {
+        b = { fg = colors.green, bg = colors.black },
+        a = { fg = colors.text_dark, bg = colors.green, gui = 'bold' },
+        c = { fg = colors.white, bg = colors.black },
+      },
+      visual = {
+        b = { fg = colors.yellow, bg = colors.black },
+        a = { fg = colors.text, bg = colors.yellow, gui = 'bold' },
+      },
+      inactive = {
+        b = { fg = colors.black, bg = colors.orange },
+        a = { fg = colors.white, bg = colors.gray, gui = 'bold' },
+      },
+      replace = {
+        b = { fg = colors.teak, bg = colors.black },
+        a = { fg = colors.text, bg = colors.teak, gui = 'bold' },
+        c = { fg = colors.white, bg = colors.black },
+      },
+      insert = {
+        b = { fg = colors.orange, bg = colors.black },
+        a = { fg = colors.text, bg = colors.orange, gui = 'bold' },
+        c = { fg = colors.white, bg = colors.black },
+      },
+    }
+    require('lualine').setup({
+      options = { theme = theme }
+    })
+  end
 
 }

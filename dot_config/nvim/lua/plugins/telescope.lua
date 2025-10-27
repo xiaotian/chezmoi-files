934d96f8539db558eeecd5bd889df3d5051ecb1a
Xiaotian Guo <guoxiaotian@gmail.com>
2025-10-27 13:05:54 -0700

    make telescope float transparent

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -7,5 +7,11 @@ return {
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+    vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg = 'none'})
+    vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg = 'none'})
+    vim.api.nvim_set_hl(0, 'TelescopePromptTitle', {bg = 'none'})
+    vim.api.nvim_set_hl(0, 'TelescopePromptBorder', {bg = 'none'})
+    vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', {bg = 'none'})
 	end
 }

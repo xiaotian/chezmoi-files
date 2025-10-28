d55a2643d1e85eee5d5a285af62949c856f33748
Xiaotian Guo <guoxiaotian@gmail.com>
2025-10-27 22:51:27 -0700

    suppress the cwd confirmation in neotree

--- dot_config/nvim/lua/plugins/neotree.lua
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -1,24 +1,24 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"nvim-tree/nvim-web-devicons", -- optional, but recommended
-	},
-	lazy = false, -- neo-tree will lazily load itself,
-	config = function()
-		vim.keymap.set('n', '<leader>n', '<cmd>Neotree toggle<CR>', { desc = 'Toggle Neo-tree' })	
-		vim.keymap.set('n', '<leader>.', '<cmd>Neotree reveal<CR>', { desc = 'Reveal current buffer in Neotree' })	
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons", -- optional, but recommended
+  },
+  lazy = false,                    -- neo-tree will lazily load itself,
+  config = function()
+    vim.keymap.set('n', '<leader>n', '<cmd>Neotree toggle<CR>', { desc = 'Toggle Neo-tree' })
+    vim.keymap.set('n', '<leader>.', '<cmd>Neotree reveal reveal_force_cwd<CR>', { desc = 'Reveal current buffer in Neotree' })
 
-        require("neo-tree").setup({
-            window = {
-                width = function()
-                    local width = math.floor(vim.o.columns * 0.25)  -- 25% of screen
-                    return math.max(10, math.min(width, 60))        -- Between 30 and 50 columns
-                end
-            }
-        })
-	end
+    require("neo-tree").setup({
+      reveal_force_cwd = true,
+      window = {
+        width = function()
+          local width = math.floor(vim.o.columns * 0.25) -- 25% of screen
+          return math.max(10, math.min(width, 60))       -- Between 30 and 50 columns
+        end
+      },
+    })
+  end
 }
-

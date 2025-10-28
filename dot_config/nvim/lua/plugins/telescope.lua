9269f1abdd3935a69541f3a8ba10d2bbb0139585
Xiaotian Guo <guoxiaotian@gmail.com>
2025-10-27 22:51:51 -0700

    added <leader>fc to search config file

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,26 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  config = function()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', '<leader>fc', function()
+      builtin.find_files {
+        cwd = vim.fn.stdpath("config"),
+        desc = 'Telescope find config files' }
+    end)
 
-    vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg = 'none'})
-    vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg = 'none'})
-    vim.api.nvim_set_hl(0, 'TelescopePromptTitle', {bg = 'none'})
-    vim.api.nvim_set_hl(0, 'TelescopePromptBorder', {bg = 'none'})
-    vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', {bg = 'none'})
-	end
+    vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { bg = 'none' })
+  end
 }

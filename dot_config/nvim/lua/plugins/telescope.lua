bf8ee52f559dc29b0bb1f1a925dd34f5a8e10b80
Xiaotian Guo <guoxiaotian@gmail.com>
2025-10-28 19:25:05 -0700

    enable telescope's ui-select for other plugins

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -4,8 +4,18 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    "nvim-telescope/telescope-ui-select.nvim",
   },
   config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({}),
+        },
+      },
+    })
+    telescope.load_extension("ui-select")
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })

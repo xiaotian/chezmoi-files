a1a2d372c4da24d5d046b82e275d4b5c5c2925b8
Xiaotian Guo <guoxiaotian@gmail.com>
2025-11-07 07:18:10 -0800

    added friendly-snippets

--- dot_config/nvim/lua/plugins/luasnip.lua
+++ dot_config/nvim/lua/plugins/luasnip.lua
@@ -1,16 +1,20 @@
 return {
-  "L3MON4D3/LuaSnip",
-  -- follow latest release.
-  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-  -- install jsregexp (optional!).
-  -- build = "make install_jsregexp"
-  config = function()
-    local ls = require("luasnip")
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+        "rafamadriz/friendly-snippets"
+    },
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    -- build = "make install_jsregexp"
+    config = function()
+        local ls = require("luasnip")
 
-    -- Path to your snippet folder
-    local snippet_path = vim.fn.stdpath("config") .. "/lua/snippets"
+        -- Path to your snippet folder
+        local snippet_path = vim.fn.stdpath("config") .. "/lua/snippets"
 
-    -- Load all *.lua files from the folder
-    require("luasnip.loaders.from_lua").lazy_load({ paths = snippet_path })
-  end
+        -- Load all *.lua files from the folder
+        require("luasnip.loaders.from_lua").lazy_load({ paths = snippet_path })
+        require("luasnip.loaders.from_vscode").lazy_load()
+    end
 }

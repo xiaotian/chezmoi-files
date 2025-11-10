2d7f92e93565009f37d3a3d9eaaea11f00863ce5
Xiaotian Guo <guoxiaotian@gmail.com>
2025-11-10 09:44:50 -0800

    add keymap to toggle git sign

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,10 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require('gitsigns').setup()
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {desc="Preview hunk"})
-    vim.keymap.set("n", "<leader>gtb", ":Gitsigns toggle_current_line_blame<CR>", {desc="Toggle current line blame"})
-  end
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require('gitsigns').setup()
+        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview hunk" })
+        vim.keymap.set("n", "<leader>gtb", ":Gitsigns toggle_current_line_blame<CR>", { desc =
+        "Toggle current line blame" })
+        vim.keymap.set('n', '<leader>vg', ':Gitsigns toggle_signs<CR>', { desc = "Toggle GitSign Column" })
+    end
 }

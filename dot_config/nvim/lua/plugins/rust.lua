bfd554797da1c89f188ad923b4d3ed4e04e60aa9
Xiaotian Guo <guoxiaotian@gmail.com>
2025-12-30 10:28:05 -0800

    added keymap to run Rust binaries.

--- dot_config/nvim/lua/plugins/rust.lua
+++ dot_config/nvim/lua/plugins/rust.lua
@@ -36,5 +36,19 @@ return {
       end
       vim.cmd('RustLsp debug {args[]}?')
     end, { desc = 'Rust debuggables' })
+
+    vim.keymap.set("n", "<leader>rl", function()
+      if vim.bo.modified then
+        vim.cmd('write')
+      end
+      vim.cmd.RustLsp({'runnables', bang=true})
+    end, { desc = 'Run Last Rust Target' })
+
+    vim.keymap.set("n", "<leader>rt", function()
+      if vim.bo.modified then
+        vim.cmd('write')
+      end
+      vim.cmd.RustLsp('runnables')
+    end, { desc = 'Select target to run' })
   end
 }

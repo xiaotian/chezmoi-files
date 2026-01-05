59744e9b57616a2deeafd37e473b4abf5f554a46
Xiaotian Guo <guoxiaotian@gmail.com>
2026-01-05 11:35:47 -0800

    fix a bug which causes lsp not attaching in 3rd party crates

--- dot_config/nvim/lua/plugins/rust.lua
+++ dot_config/nvim/lua/plugins/rust.lua
@@ -5,14 +5,22 @@ return {
     },
     version = '^6', -- Recommended
     lazy = false,   -- This plugin is already lazy
-    ft = { 'rust' },
-    ["rust-analyzer"] = {
-        cargo = {
-            allFeatures = true,
+    init = function()
+        -- Configure rustaceanvim before it loads
+        vim.g.rustaceanvim = {
+            server = {
+                settings = {
+                    ['rust-analyzer'] = {
+                        cargo = {
+                            allFeatures = true,
+                        },
+                    },
+                },
+            },
         }
-    },
+    end,
     config = function()
-        -- code actions, help float are already defined in lsp.lua
+        -- Keymaps for Rust-specific commands
         vim.keymap.set("n", "<leader>dr", function()
             if vim.bo.modified then
                 vim.cmd('write')
@@ -21,9 +29,9 @@ return {
         end, { desc = 'Rust debuggables' })
 
         vim.keymap.set("n", "<leader>rl", function()
-            -- if vim.bo.modified then
-            --   vim.cmd('write')
-            -- end
+            if vim.bo.modified then
+                vim.cmd('write')
+            end
             vim.cmd.RustLsp({ 'runnables', bang = true })
         end, { desc = 'Run Last Rust Target' })
 

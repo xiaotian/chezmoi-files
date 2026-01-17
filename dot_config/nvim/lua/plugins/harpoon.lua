b61cd9e609e484168c067c7b9a47be7a67536214
Xiaotian Guo <guoxiaotian@gmail.com>
2026-01-17 10:55:49 -0800

    added opencode plugin

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon - Add" })
+    vim.keymap.set("n", "<leader>A", function() harpoon:list():add() end, { desc = "Harpoon - Add" })
     vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
       { desc = "Harpoon - List" })
 

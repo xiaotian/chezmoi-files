85458050214c38a59595c61a3bec8434d8822fba
Xiaotian Guo <guoxiaotian@gmail.com>
2026-01-27 23:20:45 -0800

    attempt to fix the weird after new line tab jump around problem

--- dot_config/nvim/lua/plugins/luasnip.lua
+++ dot_config/nvim/lua/plugins/luasnip.lua
@@ -10,6 +10,14 @@ return {
     config = function()
         local ls = require("luasnip")
 
+        ls.setup({
+            -- Clean up stale snippet sessions when re-entering insert mode
+            -- or leaving insert mode. This prevents the super-tab <Tab> mapping
+            -- from jumping to old snippet placeholders on unrelated lines.
+            region_check_events = "InsertEnter",
+            delete_check_events = "InsertLeave",
+        })
+
         -- Path to your snippet folder
         local snippet_path = vim.fn.stdpath("config") .. "/lua/snippets"
 
